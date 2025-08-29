759e9a0513bf587900916cc88174658f4c7b3e44
zsolt-marta-bitrise <zsolt.marta@bitrise.io>
2025-08-29 16:40:26 +0200

    feat: ACI-3938 Activate xcode step (#2)

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# bitrise-step-xcode-enable-compilation-cache
+# Bitrise Build Cache Add-On for Xcode
+
+Enables the Bitrise Build Cache Add-On for Xcode.
+
+## What this step does
+
+- Configures the build using the Build Cache CLI so Bitrise Remote Build Cache is used.
+- Ensures all subsequent Xcode builds in the workflow will read from the remote cache and push new entries.
+- Adds a PATH entry to make the CLI available in all subsequent steps.
+    - From this point on, all calls to `xcodebuild` are wrapped so the underlying Xcode command has compilation caching enabled.
+    - The wrapper script in in `~/.bitrise-xcelerate/bin` and it's added as `xcodebuild` to the path in `~/.zshrc` and `~/.bashrc`,
+    - The PATH is also exported through `envman` so it's available in subsequent steps.
+- Saves analytical data (command, duration, cache information, environment) to Bitrise. The data is available on the Build cache page: https://app.bitrise.io/build-cache
+
+## Notes
+
+- Xcode 26 Beta 4 is required to use the remote cache.
+- The wrapper in the PATH persists only for subsequent steps in the current workflow run.
+- Ensure your workflow uses `xcodebuild` (or compatible tooling) to benefit from the remote cache.
---
+++ step.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+# 'read' has to be before 'set -e'
+read -r -d '' UNAVAILABLE_MESSAGE << EOF_MSG
+Bitrise Build Cache is not activated in this build.
+
+You have added the **Activate Bitrise Build Cache for Xcode** add-on step to your workflow.
+
+However, you don't have an activate Bitrise Build Cache Trial or Subscription for the current workspace yet.
+
+You can activate a Trial at [app.bitrise.io/build-cache](https://app.bitrise.io/build-cache),
+or contact us at [support@bitrise.io](mailto:support@bitrise.io) to activate it.
+EOF_MSG
+
+set -eo pipefail
+
+echo "Checking whether Bitrise Build Cache is activated for this workspace ..."
+if [ "$BITRISEIO_BUILD_CACHE_ENABLED" != "true" ]; then
+  printf "\n%s\n" "$UNAVAILABLE_MESSAGE"
+  set -x
+  bitrise plugin install https://github.com/bitrise-io/bitrise-plugins-annotations.git
+  bitrise :annotations annotate "$UNAVAILABLE_MESSAGE" --style error || {
+    echo "Failed to create annotation"
+    exit 3
+  }
+  exit 2
+fi
+echo "Bitrise Build Cache is activated in this workspace, configuring the build environment ..."
+
+# download the Bitrise Build Cache CLI
+export BITRISE_BUILD_CACHE_CLI_VERSION="v1.0.0"
+curl --retry 5 -m 30 -sSfL 'https://raw.githubusercontent.com/bitrise-io/bitrise-build-cache-cli/main/install/installer.sh' | sh -s -- -b /tmp/bin -d $BITRISE_BUILD_CACHE_CLI_VERSION || true
+
+# Fall back to Artifact Registry if the download failed
+if [ ! -f /tmp/bin/bitrise-build-cache ]; then
+  echo "Failed to download Bitrise Build Cache CLI, trying Artifact Registry ..."
+
+  version="${BITRISE_BUILD_CACHE_CLI_VERSION#v}"
+  os=$(uname -s | tr '[:upper:]' '[:lower:]')
+  arch=$(uname -m | sed 's/x86_64/amd64/' | sed 's/aarch64/arm64/')
+  package="bitrise-build-cache_${os}_${arch}.tar.gz"
+  filename="bitrise-build-cache_${version}_${os}_${arch}.tar.gz"
+
+  filepath="$package:$version:$filename"
+
+  echo "Downloading Bitrise Build Cache CLI from Artifact Registry: ${filepath}"
+
+  curl --retry 5 -m 60 -sSfL "https://artifactregistry.googleapis.com/download/v1/projects/ip-build-cache-prod/locations/us-central1/repositories/build-cache-cli-releases/files/${filepath}:download?alt=media" -o $package
+  tar -xzf "$package"
+  mkdir -p /tmp/bin
+  mv "bitrise-build-cache" /tmp/bin/bitrise-build-cache
+  rm -rf "$package"
+fi
+
+if [ ! -f /tmp/bin/bitrise-build-cache ]; then
+  echo "Failed to download Bitrise Build Cache CLI, exiting."
+  exit 1
+fi
+
+if [ "$verbose" != "true" ] && [ "$verbose" != "false" ]; then
+  echo "Parsing inputs failed: Verbose logging ($verbose) is not a valid option."
+fi
+
+# run the Bitrise Build Cache CLI
+/tmp/bin/bitrise-build-cache activate xcode --debug="$verbose" --cache
